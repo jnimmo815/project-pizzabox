a80711a52b819916a9f206b6eff4df37147a7f8b
james nimmo <Tristan_Nimmo@msn.com>
2021-03-31 17:22:36 -0400

    Starting project

---
+++ PizzaBox.Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PizzaBox.Client
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      Program.Run();
+    }
+
+    private static void Run()   //  Modifiers = static vs const vs readonly
+    {
+      System.Console.WriteLine("Welcome to PizzaBox");
+      var stores = new List<string>()
+      {
+        "Store 001",
+        "Store 002",
+        "Store 003"
+      };
+
+      foreach (var item in stores)
+      {
+        System.Console.WriteLine(item);
+      }
+    }
+  }
+}
