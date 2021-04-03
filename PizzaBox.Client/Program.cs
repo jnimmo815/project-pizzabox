e735528eb807357ecf7a6c9407971c544196bf2b
james nimmo <Tristan_Nimmo@msn.com>
2021-04-03 19:01:04 -0400

    my test

--- PizzaBox.Client/Program.cs
+++ PizzaBox.Client/Program.cs
@@ -1,29 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
+using PizzaBox.Domain.Abstracts;
+using PizzaBox.Domain.Models;
+using PizzaBox.Client.Singletons;
+using PizzaBox.Storing.Repositories;
 
 namespace PizzaBox.Client
 {
+  /// <summary>
+  /// 
+  /// </summary>
   internal class Program
   {
+    private static readonly StoreSingleton _storeSingleton = StoreSingleton.Instance;
+    private static readonly PizzaSingleton _pizzaSingleton = PizzaSingleton.Instance;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
     private static void Main(string[] args)
     {
-      Program.Run();
+      Run();
     }
 
-    private static void Run()   //  Modifiers = static vs const vs readonly
+    /// <summary>
+    /// 
+    /// </summary>
+    private static void Run()
     {
-      System.Console.WriteLine("Welcome to PizzaBox");
-      var stores = new List<string>()
-      {
-        "Store 001",
-        "Store 002",
-        "Store 003"
-      };
 
-      foreach (var item in stores)
+      var order = new Order();
+
+      Console.WriteLine("Welcome to PizzaBox");
+
+      order.Customer = new Customer();
+      order.Store = SelectStore();
+      order.Pizza = SelectPizza();
+
+      order.Save();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private static void DisplayOrder(APizza pizza)
+    {
+      Console.WriteLine($"Your order is: {pizza}");
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private static void DisplayPizzaMenu()
+    {
+      var index = 0;
+
+      foreach (var item in _pizzaSingleton.Pizzas)
       {
-        System.Console.WriteLine(item);
+        Console.WriteLine($"{++index} - {item}");
       }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    private static void DisplayStoreMenu()
+    {
+      var index = 0;
+
+      foreach (var item in _storeSingleton.Stores)
+      {
+        Console.WriteLine($"{++index} - {item}");
+      }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private static APizza SelectPizza()
+    {
+      var input = int.Parse(Console.ReadLine());
+      var pizza = _pizzaSingleton.Pizzas[input - 1];
+
+      DisplayOrder(pizza);
+
+      return pizza;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    private static AStore SelectStore()
+    {
+      var input = int.Parse(Console.ReadLine()); // be careful (think execpetion/error handling)
+
+      DisplayPizzaMenu();
+
+      return _storeSingleton.Stores[input - 1];
     }
   }
 }
---
+++ PizzaBox.Domain/Abstracts/AStore.cs
@@ -0,0 +1,12 @@
+namespace PizzaBox.Domain.Abstracts
+{
+  public abstract class AStore
+  {
+    // field
+    // public string Name;
+
+    public string Name { get; set; }
+
+  }
+
+}
