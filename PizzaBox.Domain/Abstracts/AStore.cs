3f8b2cb87b8da86bde06983d822fa2fb27522a0b
james nimmo <Tristan_Nimmo@msn.com>
2021-04-05 10:39:21 -0400

    Starting point

--- PizzaBox.Domain/Abstracts/AStore.cs
+++ PizzaBox.Domain/Abstracts/AStore.cs
@@ -1,5 +1,9 @@
+using System.Xml.Serialization;
+using PizzaBox.Domain.Models;
 namespace PizzaBox.Domain.Abstracts
 {
+  [XmlInclude(typeof(ChicagoStore))]
+  [XmlInclude(typeof(NewYorkStore))]
   public abstract class AStore
   {
     // field
